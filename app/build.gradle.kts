efef0ac3292c8f51dd4c7de47ce59b55cac37da4
jeremia49 <39850621+jeremia49@users.noreply.github.com>
2025-07-25 18:10:12 +0700

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,14 +9,14 @@ plugins {
 
 android {
     namespace = "my.id.jeremia.potholetracker"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "my.id.jeremia.potholetracker"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
